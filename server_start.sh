3769e39be61500a672825fa21add6920c8debd7f
Sagar Mali <sagarmali24@gmail.com>
2019-01-18 10:30:22 +0530

    updated file

--- server_start.sh
+++ server_start.sh
@@ -1,3 +1,3 @@
 echo 'Starting my app'
 cd '/home/ec2-user/server'
-java -jar spring-boot-demo-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-user/server/chat-control-center/target/spring-boot-demo-0.0.1-SNAPSHOT.jar
