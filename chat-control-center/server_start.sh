73151ecc7765a5474e7fe1716fe305e0bea68693
sagarm <sagarm@XIPL9155.xpanxion.co.in>
2019-01-24 08:53:06 +0530

    changed application port to 9098 & removed nohup command

--- chat-control-center/server_start.sh
+++ chat-control-center/server_start.sh
@@ -1,2 +1,2 @@
 echo 'Starting my app'
-nohup java -jar /home/ec2-user/server/chat-control-center/target/spring-boot-demo-0.0.1-SNAPSHOT.jar &
+java -jar /home/ec2-user/server/chat-control-center/target/spring-boot-demo-0.0.1-SNAPSHOT.jar &
