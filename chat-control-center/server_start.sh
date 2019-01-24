23b4f9a283a69eff5b8c63a08caa1196b05b6f09
sagarm <sagarm@XIPL9155.xpanxion.co.in>
2019-01-24 08:00:10 +0530

    added nohup command for running process in background

--- chat-control-center/server_start.sh
+++ chat-control-center/server_start.sh
@@ -1,3 +1,2 @@
 echo 'Starting my app'
-cd '/home/ec2-user/server'
-java -jar /home/ec2-user/server/chat-control-center/target/spring-boot-demo-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/server/chat-control-center/target/spring-boot-demo-0.0.1-SNAPSHOT.jar &
